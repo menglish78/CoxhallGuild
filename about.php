44bea1b6c10dd9989813025a98543eac1ac15e89
Matt English <menglish78@bellsouth.net>
2016-06-28 21:04:38 -0400

    Added content to "about.php" and "contact.php" pages.

--- about.php
+++ about.php
@@ -5,6 +5,14 @@ include("inc/header.php"); ?>
 
 <div class="page-content">
   <h2>About the Coxhall Guild</h2>
+  <p>The Guild was established in 2008.</p>
+  <p>Our mission is to:</p>
+  <ul>
+    <li>Promote social activities in Coxhall Gardens.</li>
+    <li>Enhance the overall appearance of the Coxhall Mansion.</li>
+    <li>Sponsor activities that raise funds for improvement of the property.</li>
+    <li>Provide leadership in educating the public about this historic property.</li>
+  </ul>
 </div>
 
 <?php include("inc/footer.php"); ?>
--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@ include("inc/header.php"); ?>
 
 <div class="page-content">
   <h2>Contact Us</h2>
+  <p>Sharon Terry, President</p>
+  <a href="mailto:sharontterry@gmail.com">sharontterry@gmail.com</a>
 </div>
 
 <?php include("inc/footer.php"); ?>
