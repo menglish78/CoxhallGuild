789f0737d0ee2421198562001ba9cfd9a4090758
Matt English <menglish78@bellsouth.net>
2016-07-09 13:18:49 -0400

    Added "foreach" loop for events. Echoing each event with line breaks. Not sure that this is the most efficient way to do this.

--- calendar.php
+++ calendar.php
@@ -8,6 +8,18 @@ include("inc/header.php"); ?>
 
 <div class="page-content">
   <h2>Event Calendar</h2>
+    <div class="calendar">
+      <?php
+      foreach($calendar as $events){
+        echo $events[0];
+        echo "<br>";
+        echo $events[1];
+        echo "<br>";
+        echo $events[2];
+        echo "<br>";
+      }
+      ?>
+    </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
